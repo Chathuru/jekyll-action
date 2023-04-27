86fd7c2b3f403e5e4e71fbe53bb379ce62762ec9
chathura <chathurams.sa@gmail.com>
2023-04-27 18:40:39 +0100

    Add config file change support

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 
 echo "Starting the Jekyll Action"
 
-bundle exec jekyll build -s ${GITHUB_WORKSPACE} --verbose
+export CONFIG_FILES=${1}
+
+bundle exec jekyll build -s ${GITHUB_WORKSPACE} --config $CONFIG_FILES --verbose
 
 exit $?
